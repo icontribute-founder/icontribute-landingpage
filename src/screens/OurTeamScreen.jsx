98d8ea44f8ca63dab94da7e5fcac95074dc85ae8
Omkaar Kamath <omkaarmukundkamath@gmail.com>
2021-07-06 09:44:05 -0400

    Fixed bugs

--- src/screens/OurTeamScreen.jsx
+++ src/screens/OurTeamScreen.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import ReactGA from 'react-ga';
+import React, { useEffect, useState } from "react";
+import ReactGA from "react-ga";
 
-import MainBlock from '../components/ourteam/MainBlock/MainBlock';
-import AcrossGlobeBlock from '../components/ourteam/AcrossGlobeBlock/AcrossGlobeBlock';
-import TeamBlock from '../components/ourteam/TeamBlock/TeamBlock';
-import TeamCallBlock from '../components/ourteam/TeamCallBlock/TeamCallBlock';
+import MainBlock from "../components/ourteam/MainBlock/MainBlock";
+import AcrossGlobeBlock from "../components/ourteam/AcrossGlobeBlock/AcrossGlobeBlock";
+import TeamBlock from "../components/ourteam/TeamBlock/TeamBlock";
+import TeamCallBlock from "../components/ourteam/TeamCallBlock/TeamCallBlock";
 
-import fetchTeam from '../queries/TeamMembers';
+import fetchTeam from "../queries/TeamMembers";
 
 const OurTeamScreen = () => {
-  const [ teamMembers, setTeamMembers ] = useState([]);
-  const [ teamLocations, setTeamLocations ] = useState([]);
+  const [teamMembers, setTeamMembers] = useState([]);
+  const [teamLocations, setTeamLocations] = useState([]);
   useEffect(() => {
-    document.title = 'iContribute - Our Team';
+    document.title = "iContribute - Our Team";
     ReactGA.pageview(window.location.hash.substring(1)); // substring excludes the hash "#"
   }, []);
 
