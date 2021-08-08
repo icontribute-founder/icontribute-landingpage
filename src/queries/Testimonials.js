1655fc4f5c146e092b6c502f041536ec693bf305
Omkaar Kamath <omkaarmukundkamath@gmail.com>
2021-08-08 17:49:34 -0400

    removed redundant code

--- src/queries/Testimonials.js
+++ src/queries/Testimonials.js
@@ -15,22 +15,26 @@ const query = `
 
 const fetchTestimonials = (setTestimonials) => {
   window
-    .fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        // Authenticate the request
-        Authorization: `Bearer ${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}`
-      },
-      // send the GraphQL query
-      body: JSON.stringify({ query })
-    })
+    .fetch(
+      `https://graphql.contentful.com/content/v1/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Authenticate the request
+          Authorization: `Bearer ${process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN}`,
+        },
+        // send the GraphQL query
+        body: JSON.stringify({ query }),
+      }
+    )
     .then((response) => response.json())
     .then(({ data, errors }) => {
       if (errors) {
         console.error(errors);
       } else {
         setTestimonials(data.testimonialCollection.items);
+        console.log(data.testimonialCollection.items);
       }
     });
 };
