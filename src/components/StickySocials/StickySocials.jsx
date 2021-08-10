644b0a1cd446c8ff0222da75ce6d3944cdd2895a
Omkaar Kamath <omkaarmukundkamath@gmail.com>
2021-08-10 01:21:23 -0400

    Change scroll

--- src/components/StickySocials/StickySocials.jsx
+++ src/components/StickySocials/StickySocials.jsx
@@ -22,7 +22,7 @@ const StickySocials = () => {
       setTimeout(() => {
         socialIcons.style.animation = "appearFromLeft 1s ease forwards";
         window.addEventListener("scroll", (e) => {
-          if (window.scrollY > 3200) {
+          if (window.scrollY > 3300) {
             socialIcons.style.animation = "appearFromLeft 1s ease reverse";
           } else {
             socialIcons.style.animation = "appearFromLeft 1s ease forwards";
