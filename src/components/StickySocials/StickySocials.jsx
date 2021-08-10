c00a3364dec804085f483d8eecb97c3a780f91eb
Omkaar Kamath <omkaarmukundkamath@gmail.com>
2021-08-10 01:17:05 -0400

    Added animations

--- src/components/StickySocials/StickySocials.jsx
+++ src/components/StickySocials/StickySocials.jsx
@@ -1,20 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import fb from "../../images/sticky-socials/facebook.png";
 import insta from "../../images/sticky-socials/instagram.png";
 import linkedin from "../../images/sticky-socials/linkedin.png";
 import tiktok from "../../images/sticky-socials/tiktok.png";
+import fb_onhover from "../../images/sticky-socials/facebook_onhover.png";
+import insta_onhover from "../../images/sticky-socials/instagram_onhover.png";
+import linkedin_onhover from "../../images/sticky-socials/linkedin_onhover.png";
+import tiktok_onhover from "../../images/sticky-socials/tiktok_onhover.png";
 
 import "./StickySocials.css";
 
 const StickySocials = () => {
-  const socialbar = document.querySelector(".sticky-socials");
+  const [hoverFb, setHoverFb] = useState(false);
+  const [hoverInsta, setHoverInsta] = useState(false);
+  const [hoverLinkedin, setHoverLinkedin] = useState(false);
+  const [hoverTiktok, setHoverTiktok] = useState(false);
   useEffect(() => {
-    window.addEventListener("wheel", (e) => {
-      console.log(e.deltaY > 0);
-    });
+    const navSlide = () => {
+      const socialIcons = document.querySelector(".sticky-socials");
+      setTimeout(() => {
+        socialIcons.style.animation = "appearFromLeft 1s ease forwards";
+        window.addEventListener("scroll", (e) => {
+          if (window.scrollY > 3000) {
+            socialIcons.style.animation = "appearFromLeft 1s ease reverse";
+          } else {
+            socialIcons.style.animation = "appearFromLeft 1s ease forwards";
+          }
+        });
+      }, 100);
+    };
+    navSlide();
   }, []);
-
   return (
     <div className="sticky-socials">
       <a
@@ -22,28 +39,64 @@ const StickySocials = () => {
         rel="noopener noreferrer"
         href="https://www.facebook.com/icontribute.community"
       >
-        <img alt="" src={fb} />
+        <img
+          onMouseEnter={() => {
+            setHoverFb(true);
+          }}
+          onMouseLeave={() => {
+            setHoverFb(false);
+          }}
+          alt=""
+          src={hoverFb ? fb_onhover : fb}
+        />
       </a>
       <a
         target="_blank"
         rel="noopener noreferrer"
         href="https://www.linkedin.com/company/icontribute/"
       >
-        <img alt="" src={linkedin} />
+        <img
+          onMouseEnter={() => {
+            setHoverLinkedin(true);
+          }}
+          onMouseLeave={() => {
+            setHoverLinkedin(false);
+          }}
+          alt=""
+          src={hoverLinkedin ? linkedin_onhover : linkedin}
+        />
       </a>
       <a
         target="_blank"
         rel="noopener noreferrer"
         href="https://www.instagram.com/icontribute.community/"
       >
-        <img alt="" src={insta} />
+        <img
+          onMouseEnter={() => {
+            setHoverInsta(true);
+          }}
+          onMouseLeave={() => {
+            setHoverInsta(false);
+          }}
+          alt=""
+          src={hoverInsta ? insta_onhover : insta}
+        />
       </a>
       <a
         target="_blank"
         rel="noopener noreferrer"
         href="https://www.tiktok.com/@icontribute"
       >
-        <img alt="" src={tiktok} />
+        <img
+          onMouseEnter={() => {
+            setHoverTiktok(true);
+          }}
+          onMouseLeave={() => {
+            setHoverTiktok(false);
+          }}
+          alt=""
+          src={hoverTiktok ? tiktok_onhover : tiktok}
+        />
       </a>
     </div>
   );
