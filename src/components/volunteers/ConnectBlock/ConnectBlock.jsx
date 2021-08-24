f3bf150bde81c2e653c971401028a42d3f14d51e
Omkaar Kamath <omkaarmukundkamath@gmail.com>
2021-08-24 14:49:15 -0400

    Bug fix

--- src/components/volunteers/ConnectBlock/ConnectBlock.jsx
+++ src/components/volunteers/ConnectBlock/ConnectBlock.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import phone from '../../../images/icons/Phone.png';
-import email from '../../../images/email.png';
-import './ConnectBlock.css';
+import React from "react";
+import phone from "../../../images/icons/Phone.png";
+import email from "../../../images/email.png";
+import "./ConnectBlock.css";
+import { useTranslation } from "react-i18next";
 
 const ConnectBlock = () => {
+  const { t } = useTranslation();
   return (
     <div data-aos="fade-up" className="connect-container">
       <div className="connect-wrapper">
         <div className="connect-header">
-          <h1>Connect with organizations at the tips of your fingers</h1>
-          <p>You apply to the event, and we immediately draft an email for you to reach out to the organization.</p>
+          <h1>{t("connect-header-h1")}</h1>
+          <p>{t("connect-header-p")}</p>
         </div>
         <img id="connect-icon" src={phone} alt="phone icon" />
       </div>
