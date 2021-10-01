bc10306b20927fca79f9689c921e10a420759cde
Omkaar Kamath <omkaarmukundkamath@gmail.com>
2021-09-30 22:36:32 -0400

    made copy changes

--- src/components/Navbar/EmergencyBanner.jsx
+++ src/components/Navbar/EmergencyBanner.jsx
@@ -3,24 +3,11 @@ import React, { useState } from "react";
 import "./EmergencyBanner.css";
 
 const EmergencyBanner = ({ isMobile }) => {
-  if (isMobile) {
-    return (
-      <span className="emergency-banner">
-        <div>
-          We are getting reports of an error with signing up. If you experience
-          this, please{"\u00A0"}
-          {"\u00A0"}
-          {"\u00A0"}
-          <a href="https://forms.gle/isrtWxjAberRN2BbA">Report a Bug</a>
-        </div>
-      </span>
-    );
-  }
   return (
     <span className="emergency-banner">
       <div>
-        We are getting reports of an error with signing up. If you experience
-        this, please{"\u00A0"}
+        Our mobile app is under maintenance - if you experience errors, please
+        {"\u00A0"}
         <a href="https://forms.gle/isrtWxjAberRN2BbA">Report a Bug</a>
       </div>
     </span>
