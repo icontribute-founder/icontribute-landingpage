d0c1771ee877992496a0fe1bf9dda79a95a93122
Omkaar Kamath <omkaarmukundkamath@gmail.com>
2021-10-20 19:14:08 -0400

    Added functional counters

--- src/components/home/MainBlock/MainBlock.jsx
+++ src/components/home/MainBlock/MainBlock.jsx
@@ -6,6 +6,7 @@ import { useTranslation } from "react-i18next";
 import "../../../translations/i18n";
 import "./MainBlock.css";
 import i18next from "i18next";
+import CountUp from "react-countup";
 
 import apple_en from "../../../images/apple_en.png";
 import google_en from "../../../images/google_en.png";
@@ -21,50 +22,79 @@ const MainBlock = () => {
   const isFrench = i18next.language === "FR" ? true : false;
 
   return (
-    <div data-aos={isMobile ? "" : "fade-up"} className="Home-MainBlock">
-      <div className="Home-MainBlock-Info">
-        <h1 className="Home-MainBlock-Header">{t("Home-MainBlock-Header")}</h1>
-        <p className="Home-MainBlock-Details">{t("Home-MainBlock-Details")}</p>
-        <a
-          href="https://apps.apple.com/ca/app/icontribute/id1524895764?ign-mpt=uo%3D4"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="Home-MainBlock-AppstoreLink"
-        >
-          <img src={isFrench ? apple_fr : apple_en} alt="App Store download" />
-        </a>
-        <a
-          href="https://play.google.com/store/apps/details?id=com.icontribute"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="Home-MainBlock-AppstoreLink"
-        >
-          <img
-            src={isFrench ? google_fr : google_en}
-            alt="Google Play download"
-          />
-        </a>
-        {isMobile ? (
-          <div>
-            <EventCardCarousel />
-            <div className="Home-MainBlock-Tips">
-              <StripedTip order={0} text={t("StripedTip-1")} />
-              <StripedTip order={1} text={t("StripedTip-2")} />
-              <StripedTip order={2} text={t("StripedTip-3")} />
+    <>
+      <div data-aos={isMobile ? "" : "fade-up"} className="Home-MainBlock">
+        <div className="Home-MainBlock-Info">
+          <h1 className="Home-MainBlock-Header">
+            {t("Home-MainBlock-Header")}
+          </h1>
+          <p className="Home-MainBlock-Details">
+            {t("Home-MainBlock-Details")}
+          </p>
+          <a
+            href="https://apps.apple.com/ca/app/icontribute/id1524895764?ign-mpt=uo%3D4"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="Home-MainBlock-AppstoreLink"
+          >
+            <img
+              src={isFrench ? apple_fr : apple_en}
+              alt="App Store download"
+            />
+          </a>
+          <a
+            href="https://play.google.com/store/apps/details?id=com.icontribute"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="Home-MainBlock-AppstoreLink"
+          >
+            <img
+              src={isFrench ? google_fr : google_en}
+              alt="Google Play download"
+            />
+          </a>
+          {isMobile ? (
+            <div>
+              <EventCardCarousel />
+              <div className="Home-MainBlock-Tips">
+                <StripedTip order={0} text={t("StripedTip-1")} />
+                <StripedTip order={1} text={t("StripedTip-2")} />
+                <StripedTip order={2} text={t("StripedTip-3")} />
+              </div>
             </div>
-          </div>
-        ) : (
-          ""
+          ) : (
+            ""
+          )}
+        </div>
+        {isMobile ? null : (
+          <img
+            className="Home-MainBlock-Image"
+            src={main}
+            alt="iContribute phone"
+          />
         )}
       </div>
-      {isMobile ? null : (
-        <img
-          className="Home-MainBlock-Image"
-          src={main}
-          alt="iContribute phone"
-        />
-      )}
-    </div>
+      <div className="main-counters">
+        <div className="counter">
+          <p className="counter-num">
+            <CountUp end={796} duration={5} />
+          </p>{" "}
+          <p className="counter-title">Active Users</p>
+        </div>
+        <div className="counter">
+          <p className="counter-num">
+            <CountUp end={58} duration={5} />
+          </p>{" "}
+          <p className="counter-title">Current Partners</p>
+        </div>
+        <div className="counter">
+          <p className="counter-num">
+            <CountUp end={12} duration={5} />
+          </p>{" "}
+          <p className="counter-title">Volunteers Matched</p>
+        </div>
+      </div>
+    </>
   );
 };
 
